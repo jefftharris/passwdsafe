d201342c344f90fddfd70fa49b3efea57b60a74e
Jeff Harris <jefftharris@gmail.com>
2023-11-20 17:46:26 -0500

    Gradle update

--- bin/update-gradle-metadata.sh
+++ bin/update-gradle-metadata.sh
@@ -13,5 +13,8 @@ pushd $topdir
           test \
           help \
           --dry-run
-
 mv gradle/verification-metadata.dryrun.xml gradle/verification-metadata.xml
+
+./gradlew --write-verification-metadata sha256 \
+          :lib:extractDebugAnnotations \
+          :lib:compileDebugLibraryResources
