a30d47a51046420d36b275a7032d5787f2fba2be
Jeff Harris <jefftharris@gmail.com>
2024-04-13 16:23:32 -0400

    Fix wrong comment type inspection

--- passwdsafe/src/main/java/org/pwsafe/lib/file/PwsFileV1.java
+++ passwdsafe/src/main/java/org/pwsafe/lib/file/PwsFileV1.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2008-2009 David Muller <roxon@users.sourceforge.net>.
+ * Copyright (©) 2024 Jeff Harris <jefftharris@gmail.com>
  * All rights reserved. Use of the code is allowed under the
  * Artistic License 2.0 terms, as specified in the LICENSE file
  * distributed with this code, or available from
@@ -69,12 +70,10 @@ public class PwsFileV1 extends PwsFileV1V2
         return new PwsRecordV1();
     }
 
-    /*
-     * @see org.pwsafe.lib.file.PwsFile#getBlockSize()
-     */
     @Override
     protected int getBlockSize()
     {
+        // See org.pwsafe.lib.file.PwsFile#getBlockSize()
         return 8;
     }
 }
