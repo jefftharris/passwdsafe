8ea29943aad4b686ea984f3982fa7ac5b0a6e115
Jeff Harris <jefftharris@gmail.com>
2026-06-13 16:32:08 -0400

    passwdsafe: test: Add PasswdSafeOpenFileFragmentTest
    Add basic tests for opening a password file

---
+++ passwdsafe/src/androidTest/java/com/jefftharris/passwdsafe/test/PasswdSafeOpenFileFragmentTest.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright (©) 2026 Jeff Harris <jefftharris@gmail.com>
+ * All rights reserved. Use of the code is allowed under the
+ * Artistic License 2.0 terms, as specified in the LICENSE file
+ * distributed with this code, or available from
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
+ */
+package com.jefftharris.passwdsafe.test;
+
+import android.net.Uri;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.jefftharris.passwdsafe.PasswdSafe;
+import com.jefftharris.passwdsafe.R;
+import com.jefftharris.passwdsafe.lib.PasswdSafeUtil;
+import com.jefftharris.passwdsafe.test.util.TestModeRule;
+import com.jefftharris.passwdsafe.test.util.TestUtils;
+import com.jefftharris.passwdsafe.test.util.TestV3FileRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isNotChecked;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static com.jefftharris.passwdsafe.test.util.TestUtils.withTextInputError;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.emptyString;
+
+/**
+ * UI test for PasswdSafeOpenFileFragment
+ */
+@RunWith(AndroidJUnit4.class)
+public class PasswdSafeOpenFileFragmentTest
+{
+    @Rule
+    public TestModeRule itsTestMode = new TestModeRule();
+
+    @Rule
+    public TestV3FileRule itsTestFile = new TestV3FileRule();
+
+    @Rule
+    public ActivityScenarioRule<PasswdSafe> itsActivityRule =
+            new ActivityScenarioRule<>(PasswdSafeUtil.createOpenIntent(
+                    Uri.fromFile(itsTestFile.getFile()), null));
+
+    @Test
+    public void testOpen()
+    {
+        verifyInitialFields();
+        onView(withId(R.id.passwd_edit))
+                .perform(replaceText(TestV3FileRule.PASSWD));
+        TestUtils.clickButton(R.id.open);
+        PasswdSafeTestUtils.validateOpenedEmptyFile(false);
+        PasswdSafeTestUtils.closeFile(itsActivityRule.getScenario(), false);
+    }
+
+    @Test
+    public void testEmptyPassword()
+    {
+        verifyInitialFields();
+        TestUtils.clickButton(R.id.open);
+        onView(withId(R.id.passwd_input))
+                .check(matches(withTextInputError("Invalid password")));
+    }
+
+    @Test
+    public void testBadPassword()
+    {
+        verifyInitialFields();
+        onView(withId(R.id.passwd_edit))
+                .perform(replaceText(TestV3FileRule.PASSWD + "BAD"));
+        TestUtils.clickButton(R.id.open);
+        onView(withId(R.id.passwd_input))
+                .check(matches(withTextInputError("Invalid password")));
+    }
+
+    private void verifyInitialFields()
+    {
+        onView(withId(R.id.file))
+                .check(matches(withText("Open " + itsTestFile.getFileName())));
+        onView(withId(R.id.passwd_edit))
+                .check(matches(withText(emptyString())));
+
+        onView(allOf(withId(R.id.save_password),
+                     withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+                .check(matches(isNotChecked()));
+
+        onView(withId(R.id.yubikey)).check(
+                matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+        onView(withId(R.id.yubikey_debugging)).check(
+                matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+        onView(withId(R.id.yubikey_nfc_disabled)).check(
+                matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+        onView(withId(R.id.yubikey_error)).check(
+                matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+        onView(withId(R.id.read_only_msg)).check(
+                matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+        onView(withId(R.id.saved_password)).check(
+                matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+        onView(withId(R.id.yubi_progress_text)).check(
+                matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+        onView(withId(R.id.progress)).check(matches(withEffectiveVisibility(
+                ViewMatchers.Visibility.INVISIBLE)));
+        onView(withId(R.id.open)).check(matches(withEffectiveVisibility(
+                ViewMatchers.Visibility.VISIBLE)));
+    }
+}
